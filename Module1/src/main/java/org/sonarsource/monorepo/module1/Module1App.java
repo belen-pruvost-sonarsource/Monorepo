906d3ccf43fd5ad545961c2cdc65b4e85cef4c28
belen-pruvost-sonarsource <belen.pruvost@sonarsource.com>
2021-05-28 16:58:24 +0200

    Update Module1App.java

--- Module1/src/main/java/org/sonarsource/monorepo/module1/Module1App.java
+++ Module1/src/main/java/org/sonarsource/monorepo/module1/Module1App.java
@@ -2,15 +2,17 @@ package org.sonarsource.monorepo.module1;
 
 import org.sonarsource.monorepo.library.Person;
 
+//TODO: Introduce a code smell
+
 public class Module1App {
 
   public static void main(String[] args) {
     Person person = new Person();
 
 
-      //TODO
+     
       
-      while(true) {
+      for(;;) {
 
       }
 
